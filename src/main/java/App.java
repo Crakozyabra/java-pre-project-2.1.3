190f2a474bf7e3092c99a44c20966c1f7a0f3c6d
Crakozyabra <mrcrakozyabra@gmail.com>
2023-07-19 14:44:29 +0300

    complete

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,10 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld beanAgain = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat catAgain = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == beanAgain);
+        System.out.println(cat == catAgain);
     }
 }
